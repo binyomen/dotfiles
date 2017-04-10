7957f1903af817eb76cb5c86bcfa5204c9390ae1
Ben Weedon <ben.weedon@outlook.com>
2017-04-10 12:35:50 -0700

    ps1: Add basic prompt customization

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -11,5 +11,25 @@ Import-Module posh-git
 Set-Alias l Get-ChildItem-Color -Option AllScope
 Set-Alias ls Get-ChildItem-Format-Wide -Option AllScope
 
-# Start in the home directory.
+##########################
+
+# Start in the home directory
 Set-Location ~
+
+# Customize prompt
+function prompt {
+    $origLastExitCode = $LASTEXITCODE
+
+    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+    if ($curPath.ToLower().StartsWith($Home.ToLower()))
+    {
+        $curPath = "~" + $curPath.SubString($Home.Length)
+    }
+    Write-Host ($curPath + " ") -ForegroundColor Green -NoNewline
+
+    Write-VcsStatus
+
+    $LASTEXITCODE = $origLastExitCode
+    "$('>' * ($nestedPromptLevel + 1)) "
+}
+$global:GitPromptSettings.BeforeText = '['
