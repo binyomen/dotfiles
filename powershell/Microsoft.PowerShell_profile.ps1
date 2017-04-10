8330cd4b6f48b71421c2f948ac90bc58ad06c960
Ben Weedon <ben.weedon@outlook.com>
2017-04-10 11:30:50 -0700

    ps1: Add Get-ChildItem-Color to profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,8 @@ Set-PSReadlineOption -EditMode Emacs
 
 # posh-git
 Import-Module posh-git
+
+# Get-ChildItem-Color
+. "~\Documents\WindowsPowerShell\Get-ChildItem-Color\Get-ChildItem-Color.ps1"
+Set-Alias l Get-ChildItem-Color -Option AllScope
+Set-Alias ls Get-ChildItem-Format-Wide -Option AllScope
