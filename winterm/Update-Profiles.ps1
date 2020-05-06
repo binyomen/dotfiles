4a344b19a6db816c99c5c594c885cc546834193d
Ben Weedon <ben@weedon.email>
2020-05-06 15:05:48 -0700

    Update winterm settings to newer version

--- winterm/Update-Profiles.ps1
+++
@@ -1,1 +0,0 @@
-Copy-Item -Force .\profiles.json ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
---
+++ winterm/Update-Settings.ps1
@@ -0,0 +1,1 @@
+Copy-Item -Force .\settings.json ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
