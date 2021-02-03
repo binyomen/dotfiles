3d27584b1b009be3c92fb236a7a35f8e0d86d474
Ben Weedon <ben@weedon.email>
2021-02-02 21:32:26 -0800

    Make fish git prompt "informative"

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,13 @@
 #!/usr/bin/env fish
 
 set -x PATH $PATH ~/bin
+
+# I need to add spaces after the default prompt symbols so they don't look
+# cramped in a monospace font.
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_char_stagedstate "● "
+set -g __fish_git_prompt_char_dirtystate "✚ "
+set -g __fish_git_prompt_char_conflictedstate "✖ "
+set -g __fish_git_prompt_char_cleanstate "✔ "
+set -g __fish_git_prompt_char_upstream_ahead "↑ "
+set -g __fish_git_prompt_char_upstream_behind "↓ "
