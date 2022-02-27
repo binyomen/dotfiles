e1e95c25ddb9270dc71890b00a979e9e88a97bac
Ben Weedon <ben@weedon.email>
2022-02-26 20:59:15 -0800

    Refactor scratch.lua
    It now uses more nvim API functions than plain VimL commands. The code
    for creating the buffer is a few more lines, but it technically handles
    errors better (I think?) and there's no more need for an autocmd.

--- vim/lua/scratch.lua
+++ vim/lua/scratch.lua
@@ -10,29 +10,22 @@ function M.open_scratch_buffer()
         return
     end
 
-    local bufnr = vim.fn.bufnr(SCRATCH_BUFFER_NAME)
-    if bufnr == -1 then
+    local buf = vim.fn.bufnr(SCRATCH_BUFFER_NAME)
+    if buf == -1 then
         -- The scratch buffer doesn't exist. Create it.
-        vim.cmd('edit ' .. SCRATCH_BUFFER_NAME)
-    else
-        -- The scratch buffer already exists. Switch to it.
-        vim.cmd('buffer ' .. bufnr)
+        buf = vim.api.nvim_create_buf(true --[[listed]], true --[[scratch]])
+        if buf == 0 then
+            error('Failed to create scratch buffer.')
+        end
+
+        vim.api.nvim_buf_set_name(buf, SCRATCH_BUFFER_NAME)
     end
-end
 
-function M.setup_scratch_buffer()
-    vim.opt_local.buftype = 'nofile'
-    vim.opt_local.bufhidden = 'hide'
-    vim.opt_local.swapfile = false
-    vim.opt_local.buflisted = true
+    -- The scratch buffer now exists. Switch to it. We need to use `edit`
+    -- rather than `buffer` because with buffer, the 'buflisted' option won't
+    -- be set.
+    vim.cmd(string.format('edit #%d', buf))
 end
-
-vim.cmd [[
-    augroup scratch_buffer
-        autocmd!
-        autocmd BufNewFile __SCRATCH__ lua require('scratch').setup_scratch_buffer()
-    augroup end
-]]
 
 util.map('n', '<leader>bs', ':lua require("scratch").open_scratch_buffer()<cr>')
 
