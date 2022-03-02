928a4017edc38a8d9a98f11df26cd729acbcdb5b
Ben Weedon <ben@weedon.email>
2022-03-01 21:45:03 -0800

    Don't show unlisted buffers in the tabline

--- vim/lua/statusline.lua
+++ vim/lua/statusline.lua
@@ -235,7 +235,7 @@ end
 local function render_buffers()
     local bufs = {}
     for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        if vim.api.nvim_buf_is_loaded(buf) then
+        if vim.api.nvim_buf_is_loaded(buf) and vim.fn.buflisted(buf) ~= 0 then
             table.insert(bufs, buf)
         end
     end
