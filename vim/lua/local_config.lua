90e1961fbdb2295ccbf6f8c3b2cc4eb1cbfb165a
Ben Weedon <ben@weedon.email>
2022-03-10 23:42:03 -0500

    Support marking files as trusted and untrusted
    I'm not checking checksums yet. That comes next.

--- vim/lua/local_config.lua
+++ vim/lua/local_config.lua
@@ -1,11 +1,90 @@
 local M = {}
 
+local TRUST_DIR = string.format('%s/local_config', vim.fn.stdpath('data'))
+local TRUST_FILE = string.format('%s/trust.json', TRUST_DIR)
+
+local FILE_MODE = 0b111111111 -- octal 777
+
 local CONFIG_ENV = {}
 
 _G.LOCAL_CONFIG = nil
 _G.LOADED_CONFIGS = nil
 
--- Currently only one config is supported.
+local function file_exists(file_name)
+    local s = vim.loop.fs_stat(file_name)
+    return s ~= nil and s.type == 'file'
+end
+
+local function directory_exists(file_name)
+    local s = vim.loop.fs_stat(file_name)
+    return s ~= nil and s.type == 'directory'
+end
+
+local function ensure_trust_file()
+    if not directory_exists(TRUST_DIR) then
+        assert(vim.loop.fs_mkdir(TRUST_DIR, FILE_MODE))
+    end
+
+    if not file_exists(TRUST_FILE) then
+        local f = assert(io.open(TRUST_FILE, 'w'))
+        assert(f:write('{}'))
+        f:close()
+    end
+end
+
+local function read_file(file_name)
+    ensure_trust_file()
+
+    local f = assert(io.open(file_name))
+    local contents = assert(f:read('*a'))
+    f:close()
+
+    return contents
+end
+
+local function read_trust_file()
+    local contents = read_file(TRUST_FILE)
+    return vim.json.decode(contents)
+end
+
+local function write_trust_file(o)
+    local f = assert(io.open(TRUST_FILE, 'w'))
+    assert(f:write(vim.json.encode(o)))
+    f:close()
+end
+
+local function trust(config)
+    local o = read_trust_file()
+
+    if not o[config] then
+        local contents = read_file(config)
+        o[config] = {is_trusted = true, checksum = vim.fn.sha256(contents)}
+    end
+
+    write_trust_file(o)
+end
+
+local function distrust(config)
+    local o = read_trust_file()
+
+    if not o[config] then
+        o[config] = {is_trusted = false}
+    end
+
+    write_trust_file(o)
+end
+
+local function is_trusted(config)
+    local o = read_trust_file()
+    return o[config] and o.is_trusted
+end
+
+local function is_untrusted(config)
+    local o = read_trust_file()
+    return o[config] and not o.is_trusted
+end
+
+-- Currently only one config per directory is supported.
 local function add_configs_for_dir(dir, configs)
     local file_name = string.format('%s/nvim_local.lua', dir)
     local f = io.open(file_name)
@@ -19,9 +98,16 @@ local function remove_untrusted_configs(configs)
     local final_configs = {}
 
     for _, config in ipairs(configs) do
-        local choice = vim.fn.confirm(config, '&Ignore for now\n&Trust\n&Distrust', 1)
-        if choice == 2 then
+        if is_trusted(config) then
             table.insert(final_configs, config)
+        elseif not is_untrusted(config) then
+            local choice = vim.fn.confirm(config, '&Ignore for now\n&Trust\n&Distrust', 1)
+            if choice == 2 then
+                trust(config)
+                table.insert(final_configs, config)
+            elseif choice == 3 then
+                distrust(config)
+            end
         end
     end
 
