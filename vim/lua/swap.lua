2abcf36f4c9fc86d505a47749a54335d0e51fc3b
Ben Weedon <ben@weedon.email>
2022-03-19 11:21:30 -0700

    Add basic character swapping commands

--- vim/lua/swap.lua
+++ vim/lua/swap.lua
@@ -2,6 +2,10 @@ local M = {}
 
 local util = require 'util'
 
+-- Swap current character with next and previous.
+util.map('n', '<leader>sc', [[xph]])
+util.map('n', '<leader>sC', [[xhPl]])
+
 -- Swap last delected text with the currently selected text.
 -- https://vim.fandom.com/wiki/Swapping_characters,_words_and_lines
 util.map('x', 'gx', [[<esc>`.``gvP``P``]])
