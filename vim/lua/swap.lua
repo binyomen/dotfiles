99f9d4511736dc2c66bf196217553bcb0f19312c
Ben Weedon <ben@weedon.email>
2022-03-19 12:18:43 -0700

    Make swap commands restore cursor on undo
    This is a kinda sketchy technique, but it works. I make a fake edit
    (enter insert mode, type 'a', hit backspace, then exit insert mode) when
    at the cursor position I want to restore to, and that makes undo put me
    back there.

--- vim/lua/swap.lua
+++ vim/lua/swap.lua
@@ -3,6 +3,10 @@ local M = {}
 -- Many commands here are taken from
 -- https://vim.fandom.com/wiki/Swapping_characters,_words_and_lines, with
 -- modifications by me.
+--
+-- The trick for forncing undo to restore the cursor position (see
+-- "aa<bs><esc>" in the mappings) is from
+-- https://vim.fandom.com/wiki/Restore_the_cursor_position_after_undoing_text_change_made_by_a_script.
 
 local util = require 'util'
 
@@ -12,12 +16,12 @@ util.map('n', '<leader>sc', [[xph]])
 util.map('n', '<leader>sC', [[xhPl]])
 
 -- Swap current word with the next and previous, keeping the cursor in the same place.
-util.map('n', '<leader>sw', [["_yiw:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/g<cr>``]])
-util.map('n', '<leader>sW', [["_yiwmz?\w\+\_W\+\%#<cr>:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/g<cr>`z]])
+util.map('n', '<leader>sw', [["_yiwaa<bs><esc>:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/g<cr>``]])
+util.map('n', '<leader>sW', [["_yiwmzaa<bs><esc>?\w\+\_W\+\%#<cr>:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/g<cr>`z]])
 
 -- Push the current word to the left or right.
-util.map('n', '<leader>sl', [["_yiw?\w\+\_W\+\%#<cr>:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/<cr>``]])
-util.map('n', '<leader>sr', [["_yiw:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/<cr>``/\w\+\_W\+<cr>]])
+util.map('n', '<leader>sl', [["_yiwaa<bs><esc>?\w\+\_W\+\%#<cr>:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/<cr>``]])
+util.map('n', '<leader>sr', [["_yiwaa<bs><esc>:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/<cr>``/\w\+\_W\+<cr>]])
 
 -- Swap last delected text with the currently selected text.
 -- https://vim.fandom.com/wiki/Swapping_characters,_words_and_lines
