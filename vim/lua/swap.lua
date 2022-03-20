a863a3325651593fd921aa1ec82a4af31bbad94b
Ben Weedon <ben@weedon.email>
2022-03-20 12:08:10 -0700

    Move all swap mappings to the bottom of the file

--- vim/lua/swap.lua
+++ vim/lua/swap.lua
@@ -10,19 +10,6 @@ local M = {}
 
 local util = require 'util'
 
--- Swap current character with next and previous, keeping the cursor in the
--- same place.
-util.map('n', '<leader>sc', [[xph]])
-util.map('n', '<leader>sC', [[xhPl]])
-
--- Push the current line up and down.
-util.map('n', '<m-k>', [[ddkP]])
-util.map('n', '<m-j>', [[ddp]])
-
--- Push the current paragraph up and down.
-util.map('n', '<m-[>', [[dap{{p]])
-util.map('n', '<m-]>', [[dap}p]])
-
 local swap_first_state = nil
 
 local function get_swap_reg(motion)
@@ -209,5 +196,18 @@ util.map('n', '<leader>sl', [["_yiwaa<bs><esc>?\w\+\_W\+\%#<cr>:s/\(\%#\w\+\)\(\
 util.map('n', '<leader>sr', [["_yiwaa<bs><esc>:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/<cr>``/\w\+\_W\+<cr>]])
 util.map('n', '<m-h>', '<leader>sl', {noremap = false})
 util.map('n', '<m-l>', '<leader>sr', {noremap = false})
+
+-- Swap current character with next and previous, keeping the cursor in the
+-- same place.
+util.map('n', '<leader>sc', [[xph]])
+util.map('n', '<leader>sC', [[xhPl]])
+
+-- Push the current line up and down.
+util.map('n', '<m-k>', [[ddkP]])
+util.map('n', '<m-j>', [[ddp]])
+
+-- Push the current paragraph up and down.
+util.map('n', '<m-[>', [[dap{{p]])
+util.map('n', '<m-]>', [[dap}p]])
 
 return M
