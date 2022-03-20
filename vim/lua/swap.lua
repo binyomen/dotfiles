a652229997831c86ca0896337e5ffab28bcbd1dc
Ben Weedon <ben@weedon.email>
2022-03-20 14:36:03 -0700

    Use generic swap functionality to push words
    Also fixes some swap bugs where we're returning to the wrong position by
    using extmarks.

--- vim/lua/swap.lua
+++ vim/lua/swap.lua
@@ -11,6 +11,11 @@ local M = {}
 local util = require 'util'
 
 local swap_first_state = nil
+local namespace = vim.api.nvim_create_namespace('swap')
+
+local function get_extmark_pos(extmark)
+    return vim.api.nvim_buf_get_extmark_by_id(0 --[[buffer]], namespace, extmark, {})
+end
 
 local function get_swap_reg(motion)
     local command
@@ -68,26 +73,31 @@ local function select_swap_first(motion)
     local reg = get_swap_reg(motion)
 
     local is_visual = util.is_visual_motion(motion)
-    local start_mark = is_visual and '<' or '['
-    local end_mark = is_visual and '>' or ']'
+    local start_mark_string = is_visual and '<' or '['
+    local end_mark_string = is_visual and '>' or ']'
 
-    local start_pos = {
-        line = vim.fn.line("'" .. start_mark),
-        col = vim.fn.col("'" .. start_mark) - 1,
-    }
-    local end_pos = {
-        line = vim.fn.line("'" .. end_mark),
-        col = vim.fn.col("'" .. end_mark) - 1,
-    }
+    local start_extmark = vim.api.nvim_buf_set_extmark(
+        0 --[[buffer]],
+        namespace,
+        vim.fn.line("'" .. start_mark_string) - 1,
+        vim.fn.col("'" .. start_mark_string) - 1,
+        {}
+    )
+    local end_extmark = vim.api.nvim_buf_set_extmark(
+        0 --[[buffer]],
+        namespace,
+        vim.fn.line("'" .. end_mark_string) - 1,
+        vim.fn.col("'" .. end_mark_string) - 1,
+        {}
+    )
 
     swap_first_state = {
         reg = reg,
-        pos = vim.api.nvim_win_get_cursor(0 --[[window]]),
         motion = motion,
-        start_mark = start_mark,
-        end_mark = end_mark,
-        start_pos = start_pos,
-        end_pos = end_pos,
+        start_mark_string = start_mark_string,
+        end_mark_string = end_mark_string,
+        start_extmark = start_extmark,
+        end_extmark = end_extmark,
     }
 end
 
@@ -98,30 +108,40 @@ local function perform_swap(motion)
     do_swap_put(motion, swap_first_state.reg)
 
     local pos = vim.api.nvim_win_get_cursor(0 --[[window]])
+    local extmark = vim.api.nvim_buf_set_extmark(
+        0 --[[buffer]],
+        namespace,
+        pos[1] - 1 --[[line]],
+        pos[2] --[[col]],
+        {}
+    )
 
-    vim.api.nvim_win_set_cursor(0 --[[window]], swap_first_state.pos)
+    local start_extmark = get_extmark_pos(swap_first_state.start_extmark)
+    local end_extmark = get_extmark_pos(swap_first_state.end_extmark)
     vim.api.nvim_buf_set_mark(
         0 --[[buffer]],
-        swap_first_state.start_mark,
-        swap_first_state.start_pos.line,
-        swap_first_state.start_pos.col,
+        swap_first_state.start_mark_string,
+        start_extmark[1] + 1,
+        start_extmark[2],
         {}
     )
     vim.api.nvim_buf_set_mark(
         0 --[[buffer]],
-        swap_first_state.end_mark,
-        swap_first_state.end_pos.line,
-        swap_first_state.end_pos.col,
+        swap_first_state.end_mark_string,
+        end_extmark[1] + 1,
+        end_extmark[2],
         {}
     )
 
     -- Replace the first text.
     do_swap_put(swap_first_state.motion, reg)
 
-    -- Return back to where we were.
-    vim.api.nvim_win_set_cursor(0 --[[window]], pos)
+    -- Return to our original position.
+    local extmark_pos = get_extmark_pos(extmark)
+    vim.api.nvim_win_set_cursor(0 --[[window]], {extmark_pos[1] + 1, extmark_pos[2]})
 
     swap_first_state = nil
+    vim.api.nvim_buf_clear_namespace(0 --[[buffer]], namespace, 0 --[[line_start]], -1 --[[line_end]])
 end
 
 function M.swap(motion)
@@ -149,6 +169,12 @@ local function move_word_keep_cursor(motion, opfunc, forward)
 
     local pos = vim.api.nvim_win_get_cursor(0 --[[window]])
 
+    -- If we're moving backwards move to the beginning of the word so we don't
+    -- just find our own word.
+    if not forward then
+        vim.fn.execute('normal! "_yiw')
+    end
+
     local leader = vim.g.mapleader
     vim.fn.execute(string.format('normal %sssiw', leader))
 
@@ -160,12 +186,41 @@ local function move_word_keep_cursor(motion, opfunc, forward)
     vim.api.nvim_win_set_cursor(0 --[[window]], pos)
 end
 
+local function move_word(motion, opfunc, forward)
+    if motion == nil then
+        vim.opt.opfunc = opfunc
+        return 'g@l'
+    end
+
+    local leader = vim.g.mapleader
+    vim.fn.execute(string.format('normal %sssiw', leader))
+
+    -- If we're moving backwards move to the beginning of the word so we don't
+    -- just find our own word.
+    if not forward then
+        vim.fn.execute('normal "_yiw')
+    end
+
+    local flags = forward and 'z' or 'b'
+    vim.fn.search([[\w\+]], flags)
+
+    vim.fn.execute(string.format('normal %sssiw', leader))
+end
+
 function M.next_word_keep_cursor(motion)
     return move_word_keep_cursor(motion, '__swap__next_word_keep_cursor_opfunc', true)
 end
 
 function M.previous_word_keep_cursor(motion)
     return move_word_keep_cursor(motion, '__swap__previous_word_keep_cursor_opfunc', false)
+end
+
+function M.next_word(motion)
+    return move_word(motion, '__swap__next_word_opfunc', true)
+end
+
+function M.previous_word(motion)
+    return move_word(motion, '__swap__previous_word_opfunc', false)
 end
 
 -- Currently neovim won't repeat properly if you set the opfunc to a lua
@@ -175,11 +230,19 @@ vim.cmd [[
     function! __swap__swap_opfunc(motion) abort
         return v:lua.require('swap').swap(a:motion)
     endfunction
+
     function! __swap__next_word_keep_cursor_opfunc(motion) abort
         return v:lua.require('swap').next_word_keep_cursor(a:motion)
     endfunction
     function! __swap__previous_word_keep_cursor_opfunc(motion) abort
         return v:lua.require('swap').previous_word_keep_cursor(a:motion)
+    endfunction
+
+    function! __swap__next_word_opfunc(motion) abort
+        return v:lua.require('swap').next_word(a:motion)
+    endfunction
+    function! __swap__previous_word_opfunc(motion) abort
+        return v:lua.require('swap').previous_word(a:motion)
     endfunction
 ]]
 
@@ -192,8 +255,8 @@ util.map('n', '<leader>sw', [[v:lua.require('swap').next_word_keep_cursor()]], {
 util.map('n', '<leader>sW', [[v:lua.require('swap').previous_word_keep_cursor()]], {expr = true})
 
 -- Push the current word to the left or right.
-util.map('n', '<leader>sl', [["_yiwaa<bs><esc>?\w\+\_W\+\%#<cr>:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/<cr>``]])
-util.map('n', '<leader>sr', [["_yiwaa<bs><esc>:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/<cr>``/\w\+\_W\+<cr>]])
+util.map('n', '<leader>sl', [[v:lua.require('swap').previous_word()]], {expr = true})
+util.map('n', '<leader>sr', [[v:lua.require('swap').next_word()]], {expr = true})
 util.map('n', '<m-h>', '<leader>sl', {noremap = false})
 util.map('n', '<m-l>', '<leader>sr', {noremap = false})
 
