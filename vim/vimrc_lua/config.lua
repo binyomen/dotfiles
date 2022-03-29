32d9842a4d49d2cf9f3324571fdb1468dfd66077
Ben Weedon <ben@weedon.email>
2022-03-28 21:12:26 -0700

    Fix code that used old Lua config location

--- vim/vimrc_lua/config.lua
+++ vim/vimrc_lua/config.lua
@@ -3,7 +3,7 @@ local M = {}
 local util = require 'vimrc.util'
 
 function M.complete_lua_files(--[[arg_lead, cmd_line, cursor_pos]])
-    local lua_path = string.format('%s/lua', vim.fn.stdpath('config'))
+    local lua_path = string.format('%s/lua/vimrc', vim.fn.stdpath('config'))
     local files = vim.fn.globpath(lua_path, '**/*.lua', false, true)
 
     local results = vim.tbl_map(
@@ -18,7 +18,7 @@ function M.complete_lua_files(--[[arg_lead, cmd_line, cursor_pos]])
 end
 
 function M.try_open_lua_file(file_name)
-    local lua_dir = util.normalize_path(string.format('%s/lua', vim.fn.stdpath('config')))
+    local lua_dir = util.normalize_path(string.format('%s/lua/vimrc', vim.fn.stdpath('config')))
     local full_path = string.format('%s/%s', lua_dir, file_name)
     if not util.file_exists(full_path) then
         vim.notify(string.format('File "%s" does not exist.', full_path), vim.log.levels.ERROR)
@@ -45,14 +45,14 @@ function M.lua_echo(args)
 end
 
 -- Open one of the lua configuration files.
-vim.cmd [[command! -nargs=1 -complete=custom,v:lua.package.loaded.config.complete_lua_files LFiles lua require('vimrc.config').try_open_lua_file(<q-args>)]]
+vim.cmd [[command! -nargs=1 -complete=custom,v:lua.require'vimrc.config'.complete_lua_files LFiles lua require('vimrc.config').try_open_lua_file(<q-args>)]]
 
 vim.cmd [[command! -nargs=* LEcho lua require('vimrc.config').lua_echo(<q-args>)]]
 
 util.map('n', '<leader>ve', [[<cmd>silent NormalizeEdit $MYVIMRC<cr>]])
 util.map('n', '<leader>vs', [[<cmd>lua require('vimrc.config').source()<cr>]])
 
-util.map('n', '<leader>vl', [[<cmd>execute 'silent NormalizeEdit ' . stdpath('config') . '/lua/'<cr>]])
+util.map('n', '<leader>vl', [[<cmd>execute 'silent NormalizeEdit ' . stdpath('config') . '/lua/vimrc/'<cr>]])
 util.map('n', '<leader>vp', [[<cmd>execute 'silent NormalizeEdit ' . stdpath('data') . '/site/pack/packer/'<cr>]])
 
 return M
