69bd7fe1a1755e7a764a9fe99d279429698481e3
Ben Weedon <ben@weedon.email>
2022-04-06 17:57:12 -0700

    Change init.vim to init.lua

---
+++ vim/init.lua
@@ -0,0 +1,23 @@
+require 'impatient'
+
+vim.g.mapleader = ','
+vim.g.maplocalleader = '\\'
+
+require('vimrc.local_config').load_configs()
+
+require 'vimrc.plugins'
+
+require 'vimrc.api_explorer'
+require 'vimrc.basics'
+require 'vimrc.clipboard'
+require 'vimrc.color'
+require 'vimrc.config'
+require 'vimrc.buffer'
+require 'vimrc.filetypes'
+require 'vimrc.gui'
+require 'vimrc.misc'
+require 'vimrc.search'
+require 'vimrc.statusline'
+require 'vimrc.style'
+require 'vimrc.swap'
+require 'vimrc.terminal'
