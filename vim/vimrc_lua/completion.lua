0df1d34d5f128394a2647a5155a6c4de747fa853
Ben Weedon <ben@weedon.email>
2022-04-06 19:02:48 -0700

    Support backwards search with cmp

--- vim/vimrc_lua/completion.lua
+++ vim/vimrc_lua/completion.lua
@@ -33,6 +33,9 @@ cmp.setup {
 cmp.setup.cmdline('/', {
     sources = {{name = 'buffer'}},
 })
+cmp.setup.cmdline('?', {
+    sources = {{name = 'buffer'}},
+})
 
 cmp.setup.cmdline(':', {
     sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}}),
