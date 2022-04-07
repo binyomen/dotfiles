1f5531bf5d3a621a4b6d15bee440b53ca5dbc7a4
Ben Weedon <ben@weedon.email>
2022-04-06 19:23:04 -0700

    Properly check for win32

--- vim/vimrc_lua/terminal.lua
+++ vim/vimrc_lua/terminal.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 -- Use pwsh on Windows.
-if vim.fn.has('win32') then
+if vim.fn.has('win32') == 1 then
     vim.opt.shell = 'pwsh'
     vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
     vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
