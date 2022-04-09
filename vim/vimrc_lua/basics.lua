d45ecb9f95f92d26b699cd49203da11411d2b7ba
Ben Weedon <ben@weedon.email>
2022-04-09 16:42:16 -0700

    Make context/file completion work with nvim-cmp
    I need to add an additional <c-n> to each to select the next choice.

--- vim/vimrc_lua/basics.lua
+++ vim/vimrc_lua/basics.lua
@@ -13,8 +13,8 @@ util.map('i', 'uu', '<esc>') -- Use uu to exit insert mode.
 util.map('i', 'hh', '<esc><cmd>update<cr>') -- Use hh to exit insert mode and save.
 util.map('i', '<c-h>', '<cmd>update<cr>') -- Use ctrl+h to save while in insert mode.
 util.map('i', '<c-t>', [[pumvisible() ? "\<c-n>" : "\<c-x>\<c-o>"]], {expr = true}) -- Easier omnifunc mapping.
-util.map('i', '<c-d>', '<c-x><c-f>') -- Make file completion easier.
-util.map('i', '<c-c>', '<c-x><c-n>') -- Make context-aware word completion easier.
+util.map('i', '<c-d>', '<c-x><c-f><c-n>') -- Make file completion easier.
+util.map('i', '<c-c>', '<c-x><c-n><c-n>') -- Make context-aware word completion easier.
 
 -- Movement
 util.map('', 'H', '^') -- H moves to first non-blank character of line.
