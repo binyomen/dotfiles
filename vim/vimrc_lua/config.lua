59aea43998f720ecf1336cc06149fe259744b504
Ben Weedon <ben@weedon.email>
2022-04-16 17:11:55 -0700

    Convert LFiles to use util.user_command

--- vim/vimrc_lua/config.lua
+++ vim/vimrc_lua/config.lua
@@ -1,23 +1,6 @@
-local M = {}
-
 local util = require 'vimrc.util'
 
-function M.complete_lua_files(--[[arg_lead, cmd_line, cursor_pos]])
-    local lua_path = string.format('%s/lua/vimrc', vim.fn.stdpath('config'))
-    local files = vim.fn.globpath(lua_path, '**/*.lua', false, true)
-
-    local results = vim.tbl_map(
-        function(path)
-            return vim.fn.fnamemodify(path, ':t')
-        end,
-        files)
-
-    -- Returning as a newline separate string rather than a list means vim
-    -- will handle arg_lead for us.
-    return vim.fn.join(results, '\n')
-end
-
-function M.try_open_lua_file(file_name)
+local function try_open_lua_file(file_name)
     local lua_dir = util.normalize_path(string.format('%s/lua/vimrc', vim.fn.stdpath('config')))
     local full_path = string.format('%s/%s', lua_dir, file_name)
     if not util.file_exists(full_path) then
@@ -28,8 +11,27 @@ function M.try_open_lua_file(file_name)
     vim.cmd(string.format([[vsplit %s]], full_path))
 end
 
+local LUA_PATH = string.format('%s/lua/vimrc', vim.fn.stdpath('config'))
+local function complete_lua_files(arg_lead --[[cmd_line, cursor_pos]])
+    local file_pattern = string.format('%s*.lua', arg_lead)
+    local files = vim.fn.globpath(LUA_PATH, file_pattern, false, true)
+
+    return vim.tbl_map(
+        function(path)
+            return vim.fn.fnamemodify(path, ':t')
+        end,
+        files
+    )
+end
+
 -- Open one of the lua configuration files.
-vim.cmd [[command! -nargs=1 -complete=custom,v:lua.require'vimrc.config'.complete_lua_files LFiles lua require('vimrc.config').try_open_lua_file(<q-args>)]]
+util.user_command(
+    'LFiles',
+    function (args)
+        try_open_lua_file(args.args)
+    end,
+    {nargs = 1, complete = complete_lua_files}
+)
 
 util.map('n', '<leader>ve', [[<cmd>silent NormalizeVSplit $MYVIMRC<cr>]])
 
@@ -44,5 +46,3 @@ util.map('n', '<leader>vs', source)
 
 util.map('n', '<leader>vl', [[<cmd>execute 'silent NormalizeVSplit ' . stdpath('config') . '/lua/vimrc/'<cr>]])
 util.map('n', '<leader>vp', [[<cmd>execute 'silent NormalizeVSplit ' . stdpath('data') . '/site/pack/packer/'<cr>]])
-
-return M
