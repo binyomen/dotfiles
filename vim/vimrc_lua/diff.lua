879ce9221da3833b645d71d616609b6bc5d28ae6
Ben Weedon <ben@weedon.email>
2022-04-20 01:00:15 -0700

    Add command to diff two files

---
+++ vim/vimrc_lua/diff.lua
@@ -0,0 +1,17 @@
+local util = require 'vimrc.util'
+
+-- Diff two files.
+util.user_command(
+    'Diff',
+    function(args)
+        assert(#args.fargs == 2)
+
+        local file1 = args.fargs[1]
+        local file2 = args.fargs[2]
+
+        vim.cmd [[tabedit]]
+        vim.cmd(string.format('edit %s', file1))
+        vim.cmd(string.format('vertical diffsplit %s', file2))
+    end,
+    {nargs = '+', complete = 'file'}
+)
