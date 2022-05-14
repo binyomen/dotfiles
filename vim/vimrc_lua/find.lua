85c3677e6c4507173f12744793004ac16dc579cf
Ben Weedon <ben@weedon.email>
2022-05-14 12:47:35 -0700

    Improve the find logic a bit
    Now we'll definitely only refresh the find results when in cmdline mode
    and there isn't a race condition where we might keep refreshing them
    every 10 seconds even outside cmdline mode.

--- vim/vimrc_lua/find.lua
+++ vim/vimrc_lua/find.lua
@@ -1,5 +1,6 @@
 local util = require 'vimrc.util'
 
+local in_cmdline_mode = false
 local fd_results
 local refresh_timer
 
@@ -10,41 +11,58 @@ local function cancel_refresh_timer()
     end
 end
 
-local function refresh_fd_results()
+local function refresh_fd_results(force_run)
+    local force_run = util.default(force_run, false)
+    if not in_cmdline_mode and not force_run then
+        return
+    end
+
     vim.fn.jobstart({'fd', '--strip-cwd-prefix'}, {
         stdout_buffered = true,
         on_stdout =
             function(_, output)
-                -- Cancel any current job to mark the results as stale.
+                -- Cancel any current job to refresh the results.
                 cancel_refresh_timer()
 
-                -- We need to remove the list item, since it's an empty
+                -- We need to remove the last item, since it's an empty
                 -- string representing EOF.
                 fd_results = vim.list_slice(output, 1, #output - 1)
 
-                -- Mark the results as stale after 10 seconds.
+                -- Refresh the results after 10 seconds.
                 refresh_timer = util.defer(10000, function()
-                    fd_results = nil
                     refresh_fd_results()
                 end)
             end,
     })
 end
 
+-- Give an initial value for the results even though we're not in cmdline mode.
+refresh_fd_results(true --[[force_run]])
+
 util.augroup('vimrc__fd_refresh', {
-    {'CmdlineEnter', {pattern = ':', callback = refresh_fd_results}},
-    {'CmdlineLeave', {pattern = ':', callback = function() cancel_refresh_timer() end}},
+    {'CmdlineEnter', {pattern = ':', callback =
+        function()
+            in_cmdline_mode = true
+            refresh_fd_results()
+        end,
+    }},
+    {'CmdlineLeave', {pattern = ':', callback =
+        function()
+            cancel_refresh_timer()
+            in_cmdline_mode = false
+        end,
+    }},
 })
 
 local function complete_find(arg_lead --[[cmd_line, cursor_pos]])
     if fd_results == nil then
-        return vim.fn.systemlist({'fd', '--glob', '--strip-cwd-prefix', '*' .. arg_lead .. '*'})
-    else
-        return util.filter(fd_results, function(path)
-            local pat = string.format([[\v.+%s.+]], string.gsub(arg_lead, '*', '.+'))
-            return vim.fn.match(path, pat) ~= -1
-        end)
+        return {}
     end
+
+    return util.filter(fd_results, function(path)
+        local pat = string.format([[\v.+%s.+]], string.gsub(arg_lead, '*', '.+'))
+        return vim.fn.match(path, pat) ~= -1
+    end)
 end
 
 util.user_command(
