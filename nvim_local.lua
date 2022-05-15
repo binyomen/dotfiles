838c07269d75e697dda4cf25190f18bc12a32a65
Ben Weedon <ben@weedon.email>
2022-05-15 11:35:54 -0700

    Properly set filetypes for some dotfiles

--- nvim_local.lua
+++ nvim_local.lua
@@ -1,3 +1,20 @@
 return {
     use_nvim_lua_ls = true,
+    on_config_end = function()
+        local util = require 'vimrc.util'
+
+        -- Correctly set filetypes on dotfiles.
+        local function make_filetype_autocmd(filename, filetype)
+            return {{'BufNewFile', 'BufRead'}, {pattern = filename, callback =
+                function()
+                    vim.opt_local.filetype = filetype
+                end
+            }}
+        end
+        util.augroup('nvim_local__set_dotfile_filetypes', {
+            make_filetype_autocmd('gitconfig', 'gitconfig'),
+            make_filetype_autocmd('bashrc', 'sh'),
+            make_filetype_autocmd('vimrc', 'vim'),
+        })
+    end,
 }
