e136d9068a89a0d4f1fbc5f86960822b6d3a43a2
Ben Weedon <ben@weedon.email>
2022-06-07 21:36:57 -0700

    Revert "Get rid of spaces after git symbols in prompt"
    This reverts commit ed6ead9027f1a19ef9bc96ec1dd1d584073d2f8c.
    
    Actually it's still pretty bad lol.

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,12 @@ end
 
 set -x EDITOR nvim
 
+# I need to add spaces after the default prompt symbols so they don't look
+# cramped in a monospace font.
 set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_char_stagedstate "● "
+set -g __fish_git_prompt_char_dirtystate "✚ "
+set -g __fish_git_prompt_char_conflictedstate "✖ "
+set -g __fish_git_prompt_char_cleanstate "✔ "
+set -g __fish_git_prompt_char_upstream_ahead "↑ "
+set -g __fish_git_prompt_char_upstream_behind "↓ "
