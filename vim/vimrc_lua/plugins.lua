44dab506c3e3bfe7c52e2e85a702edb962971674
Ben Weedon <ben@weedon.email>
2022-11-05 17:20:27 -0700

    Remove unused plugin

--- vim/vimrc_lua/plugins.lua
+++ vim/vimrc_lua/plugins.lua
@@ -334,7 +334,6 @@ return require('packer').startup {
                 'hrsh7th/cmp-nvim-lsp',
                 'hrsh7th/cmp-buffer',
                 'hrsh7th/cmp-path',
-                'hrsh7th/cmp-cmdline',
                 'hrsh7th/cmp-nvim-lua',
                 'hrsh7th/cmp-omni',
                 'saadparwaiz1/cmp_luasnip',
