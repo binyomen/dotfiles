f81c2b1def19152f5dc7d6c3d816c673c0c089de
Ben Weedon <ben@weedon.email>
2022-12-04 00:46:05 -0800

    Simplify setting filetypes

--- nvim_local.lua
+++ nvim_local.lua
@@ -10,22 +10,14 @@ return {
         },
     },
     on_config_end = function()
-        local util = require 'vimrc.util'
-
-        -- Correctly set filetypes on dotfiles.
-        local function make_filetype_autocmd(filename, filetype)
-            return {{'BufNewFile', 'BufRead'}, {pattern = filename, callback =
-                function()
-                    vim.bo.filetype = filetype
-                end
-            }}
-        end
-        util.augroup('nvim_local__set_dotfile_filetypes', {
-            make_filetype_autocmd('gitattributes', 'gitattributes'),
-            make_filetype_autocmd('gitconfig', 'gitconfig'),
-            make_filetype_autocmd('gitignore', 'gitignore'),
-            make_filetype_autocmd('bashrc', 'sh'),
-            make_filetype_autocmd('vimrc', 'vim'),
-        })
+        vim.filetype.add {
+            filename = {
+                gitattributes = 'gitattributes',
+                gitconfig = 'gitconfig',
+                gitignore = 'gitignore',
+                bashrc = 'sh',
+                vimrc = 'vim',
+            },
+        }
     end,
 }
