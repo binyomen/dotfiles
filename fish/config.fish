7d3aba5cb8a3a6b6237a82960f03628f5078f302
Ben Weedon <ben@weedon.email>
2023-02-23 23:05:39 -0800

    Get rid of now-irrelevant comment

--- fish/config.fish
+++ fish/config.fish
@@ -17,8 +17,6 @@ end
 
 set -x EDITOR nvim
 
-# I need to add spaces after the default prompt symbols so they don't look
-# cramped in a monospace font.
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showuntrackedfiles 1
 
