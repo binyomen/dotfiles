114bf6c94a8cef6cc9274b56a629de23da8357ea
Ben Weedon <ben@weedon.email>
2023-02-25 00:34:31 -0800

    Theme fish and kitty with tokyonight

--- fish/config.fish
+++ fish/config.fish
@@ -21,3 +21,34 @@ set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showuntrackedfiles 1
 
 alias icat='kitty +kitten icat'
+
+# See https://github.com/folke/tokyonight.nvim/blob/main/extras/fish/tokyonight_night.fish
+set -l theme_foreground c0caf5
+set -l theme_selection 33467c
+set -l theme_comment 565f89
+set -l theme_red f7768e
+set -l theme_orange ff9e64
+set -l theme_yellow e0af68
+set -l theme_green 9ece6a
+set -l theme_purple 9d7cd8
+set -l theme_cyan 7dcfff
+set -l theme_pink bb9af7
+set -g fish_color_normal $theme_foreground
+set -g fish_color_command $theme_cyan
+set -g fish_color_keyword $theme_pink
+set -g fish_color_quote $theme_yellow
+set -g fish_color_redirection $theme_foreground
+set -g fish_color_end $theme_orange
+set -g fish_color_error $theme_red
+set -g fish_color_param $theme_purple
+set -g fish_color_comment $theme_comment
+set -g fish_color_selection --background=$theme_selection
+set -g fish_color_search_match --background=$theme_selection
+set -g fish_color_operator $theme_green
+set -g fish_color_escape $theme_pink
+set -g fish_color_autosuggestion $theme_comment
+set -g fish_pager_color_progress $theme_comment
+set -g fish_pager_color_prefix $theme_cyan
+set -g fish_pager_color_completion $theme_foreground
+set -g fish_pager_color_description $theme_comment
+set -g fish_pager_color_selected_background --background=$theme_selection
