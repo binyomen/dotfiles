b17704fadb5e3368230a012a2e5a563597da9009
Ben Weedon <ben@weedon.email>
2023-05-17 13:25:28 -0700

    Set HTML text width to 80

--- vim/after/ftplugin/html.lua
+++ vim/after/ftplugin/html.lua
@@ -1,4 +1,7 @@
-require('vimrc.util').set_tab_size(2)
+local util = require 'vimrc.util'
+
+vim.bo.textwidth = 80
+util.set_tab_size(2)
 
 vim.schedule(function()
     vim.fn['textobj#quote#init'] {educate = 0}
