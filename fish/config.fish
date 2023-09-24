6dec0564cd617f90531d0cc2514a643c57d4d3c7
Ben Weedon <ben@weedon.email>
2023-09-23 18:45:28 -0700

    Modify fish colors to use tokyonight in prompt
    I can futz with these as necessary in the future.

--- fish/config.fish
+++ fish/config.fish
@@ -26,7 +26,7 @@ set -g __fish_git_prompt_showuntrackedfiles 1
 
 alias icat='kitty +kitten icat'
 
-# See https://github.com/folke/tokyonight.nvim/blob/main/extras/fish/tokyonight_night.fish
+# Based on https://github.com/folke/tokyonight.nvim/blob/main/extras/fish/tokyonight_night.fish
 set -l theme_foreground c0caf5
 set -l theme_selection 33467c
 set -l theme_comment 565f89
@@ -38,13 +38,13 @@ set -l theme_purple 9d7cd8
 set -l theme_cyan 7dcfff
 set -l theme_pink bb9af7
 set -g fish_color_normal $theme_foreground
-set -g fish_color_command $theme_cyan
+set -g fish_color_command $theme_purple
 set -g fish_color_keyword $theme_pink
 set -g fish_color_quote $theme_yellow
 set -g fish_color_redirection $theme_foreground
 set -g fish_color_end $theme_orange
 set -g fish_color_error $theme_red
-set -g fish_color_param $theme_purple
+set -g fish_color_param $theme_green
 set -g fish_color_comment $theme_comment
 set -g fish_color_selection --background=$theme_selection
 set -g fish_color_search_match --background=$theme_selection
@@ -56,3 +56,9 @@ set -g fish_pager_color_prefix $theme_cyan
 set -g fish_pager_color_completion $theme_foreground
 set -g fish_pager_color_description $theme_comment
 set -g fish_pager_color_selected_background --background=$theme_selection
+set -g fish_color_status $theme_red
+set -g fish_color_cwd $theme_cyan
+set -g fish_color_cwd_root $theme_red
+set -g fish_color_host $theme_foreground
+set -g fish_color_host_remote $theme_yellow
+set -g fish_color_user $theme_cyan
