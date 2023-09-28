e28bbfd03cf38e8a531188200d38deae8426226c
Ben Weedon <ben@weedon.email>
2023-09-28 15:11:57 -0400

    Migrate to indent blankline version 3

--- vim/vimrc_lua/plugins.lua
+++ vim/vimrc_lua/plugins.lua
@@ -153,7 +153,7 @@ return require('packer').startup {
         use {
             'lukas-reineke/indent-blankline.nvim',
             config = function()
-                require('indent_blankline').setup()
+                require('ibl').setup()
             end,
         }
         use {
