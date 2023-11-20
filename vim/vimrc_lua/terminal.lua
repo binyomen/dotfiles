d9f5887bd6528b5b51467f4fbd47266e2f83eaa6
Ben Weedon <ben@weedon.email>
2023-11-20 11:11:53 -0500

    Disable spellchecking for terminal buffers

--- vim/vimrc_lua/terminal.lua
+++ vim/vimrc_lua/terminal.lua
@@ -4,3 +4,16 @@ local util = require 'vimrc.util'
 if util.vim_has('win32') then
     util.enable_pwsh()
 end
+
+util.augroup('vimrc__terminal', {
+    {'TermOpen', {callback =
+        function()
+            -- Use `opt_local` rather than `wo` to properly mimic the behavior
+            -- of `:setlocal`. Otherwise all buffers opened in the same window
+            -- as the terminal after the terminal is opened will also have
+            -- spellchecking disabled.
+            vim.opt_local.spell = false
+        end
+    }},
+})
+
