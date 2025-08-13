ca594866d8a23b19a6ab2fd332057dca74d4e4c9
Ben Weedon <ben@weedon.email>
2025-08-12 23:30:55 -0400

    Don't automatically reflow text in insert mode for HTML
    It gets real annoying. I should have turned it off a long time ago.

--- vim/after/ftplugin/html.lua
+++ vim/after/ftplugin/html.lua
@@ -3,6 +3,9 @@ local util = require 'vimrc.util'
 vim.bo.textwidth = 80
 util.set_tab_size(2)
 
+-- Don't automatically reflow text in insert mode
+vim.opt_local.formatoptions:remove('t')
+
 vim.schedule(function()
     vim.fn['textobj#quote#init'] {educate = 0}
 end)
